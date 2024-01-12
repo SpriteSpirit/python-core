ee2eac8b3038344ca03b92da477a4d7d1adfc752
Sprite_Spirit <a.backer@yandex.com>
2024-01-12 23:59:23 +0300

    12/01/24

---
+++ Other/stuff.py
@@ -0,0 +1,23 @@
+from random import randint
+
+APPLE = {
+    'type': 'food',
+    'name': 'яблоко',
+    'price': randint(5, 10),
+    'count': 5,
+}
+
+WOODEN = {
+    'type': 'build',
+    'name': 'древесина',
+    'price': randint(1, 10),
+    'count': 5,
+}
+
+SWORD = {
+    'type': 'attack',
+    'name': 'меч',
+    'price': randint(20, 100),
+    'count': 5,
+}
+
