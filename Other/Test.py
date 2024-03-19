c47ae516aa3c65de23f861cbdf9ae4aca33042d3
Sprite_Spirit <a.backer@yandex.com>
2024-03-20 00:39:00 +0300

    12/01/24

--- Other/Test.py
+++ Other/Test.py
@@ -49,4 +49,131 @@ def multiply_range(start, end):
 
 
 print(multiply_range(5, 10))
-'''
+
+
+# пример функционального программирования
+def progression(start, stop, step):
+    if start > stop:
+        start, stop = stop, start
+
+    if start < stop:
+        print(start)
+        progression(start + step, stop, step)
+    else:
+        return
+
+
+progression(0, 10, 1)
+import turtle as t
+from random import randint, choice
+
+
+def go_snowy(length):
+
+    t.speed(100)
+    if length < 5:
+       t.forward(length)
+    else:
+        go_snowy(length/3)
+        t.left(60)
+        go_snowy(length/3)
+        t.right(120)
+        go_snowy(length/3)
+        t.left(60)
+        go_snowy(length / 3)
+
+
+for i in range(100):
+    t.up()
+    t.goto(randint(-400, 400), randint(-400, 400))
+    t.down()
+
+    for j in range(3):
+        colors = ['green', 'yellow', 'pink', 'red', 'purple', 'black', 'olive', 'blue']
+        t.color(choice(colors))
+        go_snowy(50)
+        t.right(120)
+
+t.mainloop()
+
+
+
+def double_performer(func, x):
+    return func(func(x)) # (5*10)*10
+
+
+def f(x): return x * 10
+
+
+def f2(x): return x * x
+
+
+def f3(x): return -x
+
+
+# print(double_performer(f, 5))
+
+f1 = f
+
+for f in f1, f2, f3:
+    print(double_performer(f, 5))
+
+def f(x): return x * 10
+
+
+A = (1, 2, 3, 4, 5)
+C = map(f, A)
+
+for y in C:
+    print(y)
+
+
+A = range(10)
+B = (x for x in A if x % 2 == 0)
+print(*B) # развертка
+print(*(x * x for x in A if x % 2 == 1))
+print(*map(lambda x: x*x, A))'''
+#
+# A = (1, 2, 3, 4, 5)
+# B = tuple(x * 10 for x in A)
+#
+# print(B)
+# C = zip(A, B)
+#
+# # for i in C:
+# #     print(i)
+#
+# i = iter(C)
+# next(i)
+#
+#
+# for i in C:
+#     a, b = i
+#     print(a, b, i[0] + i[1])
+
+
+# print(*map(lambda i: i[0] * i[1], enumerate('Hello'))) # e ll lll oooo
+
+# def arithm_progression(start, stop, step):
+#     x = start
+#     while x < stop:
+#         print(f'now working on x = {x}')
+#         yield x
+#         x += step
+#
+#
+# A = arithm_progression(1, 10, 2)
+#
+# for x in A:
+#     print(x)
+
+# numpy, itertools
+
+# from itertools import starmap
+#
+# print(*starmap(lambda i, char: char * i, enumerate('Hello'))) # e ll lll oooo
+
+from functools import reduce
+
+A = (1, 2, 3, 4, 5, 6)
+print(reduce(lambda x, y: x + y, A))
--- Other/test5.py
+++ Other/test5.py
@@ -1,4 +1,54 @@
-def sum_numbers(a, b):
-    return a + b
+text = """Вечер
+Как тихо веет над долиной
+Далекий колокольный звон,
+Как шорох стаи журавлиной, —
+И в шуме листьев замер он.
 
-print(sum_numbers(2, 3))
+Как море вешнее в разливе,
+Светлея, не колыхнет день, —
+И торопливей, молчаливей
+Ложится по долине тень.
+
+dict1 = {}
+list_tuple = []
+
+for word in text.split():
+    dict1[word] = dict1.get(word, 0) + 1
+
+for key, value in dict1.items():
+    list_tuple.append((value, key))
+
+
+sort_list = sorted(list_tuple, reverse=True)
+
+for item in sort_list[:5]:
+    print(item)
+
+# print(list_tuple)
+"""
+#
+#
+# def summary_numbers(num1: int, num2: int) -> int:
+#     return num1 + num2
+#
+#
+# print(summary_numbers([1, 2], ['3.8', '1.8']))
+
+
+class Cat:
+
+    def __init__(self, name, color, age):
+        self.name = name
+        self.color = color
+        self.age = age
+
+
+cat1 = Cat("Барсик", "Белый", 3)
+cat2 = Cat("Мурзик", "Чёрный", 7)
+cat3 = Cat("Василий", "Рыжий", 2)
+
+print(cat1.name, cat2.name, cat3.name)
+print(cat1.color, cat2.color, cat3.color)
+print(cat1.age, cat2.age, cat3.age)
+
+
